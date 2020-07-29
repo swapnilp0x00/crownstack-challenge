c5487b68b013feae5c789c90f4f7638bf5183496
swapnil <ddevil691@gmail.com>
2020-07-30 00:04:00 +0530

    main moved to _main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+    loadChildren: () => import('./modules/_main/main.module').then(m => m.MainModule)
   }
 ];
 @NgModule({
