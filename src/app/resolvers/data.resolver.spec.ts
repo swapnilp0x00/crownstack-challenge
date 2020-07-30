998f22829a3a58612c20425c9add773e3c9c7f7c
swapnil <ddevil691@gmail.com>
2020-07-30 21:49:15 +0530

    Added data resolver

--- src/app/resolvers/data.resolver.spec.ts
+++ src/app/resolvers/data.resolver.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DataService } from './data.resolver';
+import { DataResolver } from './data.resolver';
 
 describe('DataService', () => {
-  let service: DataService;
+  let service: DataResolver;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
+    service = TestBed.inject(DataResolver);
   });
 
   it('should be created', () => {
