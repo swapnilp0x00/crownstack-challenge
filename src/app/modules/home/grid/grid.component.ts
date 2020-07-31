ab5470b0ae8cd62aedb5724e75807acb5b54d584
swapnil <ddevil691@gmail.com>
2020-07-31 08:31:30 +0530

    Completed grid component

--- src/app/modules/home/grid/grid.component.ts
+++ src/app/modules/home/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
-
+  @Input() list: Array<any>;
   constructor() { }
 
   ngOnInit(): void {
